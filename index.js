958ebaae2a804ad7f939e28d4db9c9e8a3a43e94
Cindewi <112582932+dewismecindy@users.noreply.github.com>
2023-02-16 19:17:34 +0100

    MARVEL_API_KEY

--- index.js
+++ index.js
@@ -1,21 +1,105 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const axios = require("axios");
+// instanciation du serveur :
 const app = express();
-app.use(express.json());
 app.use(cors());
 
-const charactersRoute = require("./routes/characters");
-const comicsRoute = require("./routes/comics");
-const favoritesRoute = require("./routes/favoris");
-
-app.use(charactersRoute);
-app.use(comicsRoute);
-app.use(favoritesRoute);
-
-app.all("*", (req, res) => {
-  res.status(404).json({ error: "Not Found" });
+app.get("/", (req, res) => {
+  try {
+    // console.log(process.env.MARVEL_API_KEY);
+    return res.status(200).json("Bienvenue sur notre serveur Marvel");
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
 });
 
-app.listen(process.env.PORT, () => console.log("Server started"));
+// Une route pour récupérer les personnages en GET
+app.get("/characters", async (req, res) => {
+  try {
+    let filters = "";
+    if (req.query.name) {
+      filters = "&name=" + req.query.name;
+    }
+    let skip = "";
+    if (req.query.skip) {
+      skip = "&skip=" + req.query.skip * 100;
+    }
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}${filters}${skip}`
+    );
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+app.get("/character/:characterId", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/character/${req.params.characterId}?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+
+app.get("/related-comics", async (req, res) => {
+  try {
+    let characterID = req.query.characterID;
+    console.log(characterID);
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/character/${characterID}?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+
+// Une route pour récupérer les comics en GET
+app.get("/comics", async (req, res) => {
+  try {
+    // contacter l'API
+    const response = await axios.get(
+      "https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=" +
+        process.env.MARVEL_API_KEY
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+
+app.get("/comics", async (req, res) => {
+  try {
+    let filters = "";
+    if (req.query.name) {
+      filters = "&name=" + req.query.name;
+    }
+    let skip = "";
+    if (req.query.skip) {
+      skip = "&skip=" + req.query.skip * 100;
+      console.log(skip);
+    }
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}${filters}${skip}`
+    );
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+
+// + filtres
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Serveur is on fire 🔥, on port ${PORT}`);
+});
